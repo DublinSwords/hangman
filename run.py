6abcc3b93e23a62adeff0c3b6630f14d5c999dc6
DublinSwords <victorcotov@gmail.com>
2022-02-23 23:57:46 +0000

    added equal sign between guesses

--- run.py
+++ run.py
@@ -2,17 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random 
-#words picked for guess
+# words picked for guess
 words = ['python', 'program']
 
 picked = random.choice(words)
-#right empty list and wrong empty list
+# right empty list and wrong empty list
 right = []
 wrong = []
-#While loop with if statment 
+# While loop with if statment 
 while True:
-    """If user will guess a letter on the screen wil appear Right message 
-    if user will guess wrong letter on the screen will appear wrong message"""
+    """ In case If user will guess a letter on the screen wil appear Right message 
+    in case if user will guess wrong letter on the screen will appear wrong message"""
+    print("======================")
     guess = input("Guess a letter")
 
     if guess in picked:
