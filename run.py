cbb8f6102e27cdf22629d907ac9b56f4ec55cd10
DublinSwords <victorcotov@gmail.com>
2022-02-24 00:09:04 +0000

    added message and function update to update the right list

--- run.py
+++ run.py
@@ -2,19 +2,31 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random 
+
 # words picked for guess
 words = ['python', 'program']
 
 picked = random.choice(words)
+print("The word has", len(picked), "letter")
+
 # right empty list and wrong empty list
-right = []
+right = ['_'] * len(picked)
 wrong = []
+
+# Function will update my right list
+def update():
+    for i in right:
+        print(i, end = ' ')
+    print()
+update()
+
+
 # While loop with if statment 
 while True:
     """ In case If user will guess a letter on the screen wil appear Right message 
     in case if user will guess wrong letter on the screen will appear wrong message"""
     print("======================")
-    guess = input("Guess a letter")
+    guess = input("Guess a letter:")
 
     if guess in picked:
         right.append(guess)
